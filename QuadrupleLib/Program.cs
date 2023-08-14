4f7e8924c9276417a9908f003deccb3235e7e628
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-08-14 13:36:26 -0500

    Turn it into proper library project

--- QuadrupleLib/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using QuadrupleLib;
-
-var original = Float128.PI * Float128.PI;
-var parsed = Float128.Parse(original.ToString());
-Console.WriteLine(original);
-Console.WriteLine(parsed);
-Console.WriteLine(original == parsed);
