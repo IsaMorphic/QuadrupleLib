c02a77b46d9bddfbc16a0e278c82576b29679289
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-07-29 16:20:45 -0500

    Create driver code for testing

---
+++ QuadrupleLib.Driver/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QuadrupleLib;
+
+using StreamWriter writer = File.CreateText("cordic.csv");
+writer.WriteLine("x,sin,cos");
+for (int i = -90; i <= 90; i += 5) 
+{
+    (Float128 sin, Float128 cos) = Float128.SinCos(i * Float128.Pi / 180);
+    writer.WriteLine($"{i},{sin.ToString()},{cos.ToString()}");
+}
