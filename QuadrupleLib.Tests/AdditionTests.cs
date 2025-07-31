7dc7f3c169d2778ac2fa340683b52804b78b0837
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-07-31 12:35:10 -0500

    Feature: Implement unit testing

---
+++ QuadrupleLib.Tests/AdditionTests.cs
@@ -0,0 +1,33 @@
+namespace QuadrupleLib.Tests
+{
+    public class AdditionTests
+    {
+        [Theory]
+        [InlineData(0.5)]
+        [InlineData(1.0)]
+        [InlineData(-1.0)]
+        [InlineData(0.33)]
+        public void AddZeroIsIdentity(double x)
+        {
+            Assert.Equal(x, x + Float128.Zero);
+        }
+
+        [Theory]
+        [InlineData([0.5, 1.5])]
+        [InlineData([1.0, 2.0])]
+        [InlineData([-1.0, 0.0])]
+        public void AddOneIsCorrect(double x, double y) 
+        {
+            Assert.Equal(y, x + Float128.One);
+        }
+
+        [Theory]
+        [InlineData([0.5, 1.5, 2.0])]
+        [InlineData([1.0, 2.0, 3.0])]
+        [InlineData([-1.0, 2.0, 1.0])]
+        public void AddGeneralIsCorrect(double x, double y, double z) 
+        {
+            Assert.Equal(z, (Float128)x + y);
+        }
+    }
+}
