1f6a14a3cea8e78c306f93c0910c011338d8658a
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-08-01 14:09:20 -0500

    Fix output for CoRDiC test

--- QuadrupleLib.Tests/TrigTests.cs
+++ QuadrupleLib.Tests/TrigTests.cs
@@ -14,11 +14,11 @@ namespace QuadrupleLib.Tests
         [Fact]
         public void ComputeSinCosTable() 
         {
-            _outputHelper.WriteLine($"x{new string(' ', 2)}sin(x){new string(' ', 44)}{new string(' ', 44)}cos(x)");
+            _outputHelper.WriteLine($"{new string(' ', 2)}x{new string(' ', 42)}sin(x){new string(' ', 42)}cos(x)");
             for (int i = 0; i <= 360; i += 5)
             {
                 (Float128 sin, Float128 cos) = Float128.SinCos(i * Float128.Pi / 180);
-                _outputHelper.WriteLine($"{i,-3}{sin,50}{cos,50}");
+                _outputHelper.WriteLine($"{i,3}{sin,48}{cos,48}");
             }
         }
     }
