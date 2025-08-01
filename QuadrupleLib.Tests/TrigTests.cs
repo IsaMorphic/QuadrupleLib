b4780801744b53ef6e4b8ac1656b172e70f30c9b
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-08-01 14:02:44 -0500

    Add CoRDiC test

---
+++ QuadrupleLib.Tests/TrigTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit.Abstractions;
+
+namespace QuadrupleLib.Tests
+{
+    public class TrigTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public TrigTests(ITestOutputHelper outputHelper) 
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void ComputeSinCosTable() 
+        {
+            _outputHelper.WriteLine($"x{new string(' ', 2)}sin(x){new string(' ', 44)}{new string(' ', 44)}cos(x)");
+            for (int i = 0; i <= 360; i += 5)
+            {
+                (Float128 sin, Float128 cos) = Float128.SinCos(i * Float128.Pi / 180);
+                _outputHelper.WriteLine($"{i,-3}{sin,50}{cos,50}");
+            }
+        }
+    }
+}
