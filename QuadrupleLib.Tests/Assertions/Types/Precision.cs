81f6013f8af41478602239dcf3ec9959f4a86cda
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-12-19 15:10:31 -0600

    Feature: implement custom assertions for precision checking

---
+++ QuadrupleLib.Tests/Assertions/Types/Precision.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *  Copyright 2025 Chosen Few Software
+ *  This file is part of QuadrupleLib.
+ *
+ *  QuadrupleLib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QuadrupleLib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with QuadrupleLib.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace QuadrupleLib.Tests.Assertions.Types;
+
+internal enum Precision
+{
+    NearestUnit = 0,
+    NearestTenth = 1,
+    NearestHundreth = 2,
+    NearestThousandth = 3,
+}
