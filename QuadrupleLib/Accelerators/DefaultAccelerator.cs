a8de63f09927a5efa8c6b532712f2f0667f4c06c
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-12-20 15:03:20 -0600

    Fix: include copyright headers on added files

--- QuadrupleLib/Accelerators/DefaultAccelerator.cs
+++ QuadrupleLib/Accelerators/DefaultAccelerator.cs
@@ -1,4 +1,22 @@
-﻿namespace QuadrupleLib.Accelerators;
+﻿/*
+ *  Copyright 2024-2026 Chosen Few Software
+ *  This file is part of QuadrupleLib.
+ *
+ *  QuadrupleLib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QuadrupleLib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with QuadrupleLib.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace QuadrupleLib.Accelerators;
 
 public sealed class DefaultAccelerator : IAccelerator
 {
