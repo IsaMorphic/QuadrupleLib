5aadfb9cb5e4c48a0fbedb83bd65f5ebb66065da
Isabelle Santin <isabelle@chosenfewsoftware.com>
2026-03-13 12:51:23 -0500

    Feature: add tests for logarithms

---
+++ QuadrupleLib.Tests/Math/LogarithmTests.cs
@@ -0,0 +1,105 @@
+﻿using QuadrupleLib.Tests.Assertions.Types;
+using Xunit;
+
+namespace QuadrupleLib.Tests.Math
+{
+    public class LogarithmTests
+    {
+        [Theory]
+        [InlineData(-0.288)]
+        [InlineData(0.5)]
+        [InlineData(2.0)]
+        [InlineData(-2.0)]
+        [InlineData(-4.65)]
+        public void IsLogCorrect(double x)
+        {
+            double y = double.Exp(x);
+            AssertX.NearlyEqual(x, Quad.Log(y), Precision.NearestThousandth);
+        }
+
+        [Theory]
+        [InlineData(0.0)]
+        [InlineData(-1.0)]
+        public void IsLogNaN(double x) 
+        {
+            Quad y = Quad.Log(x);
+            Assert.True(Quad.IsNaN(y));
+        }
+
+        [Theory]
+        [InlineData(-0.288)]
+        [InlineData(0.5)]
+        [InlineData(2.0)]
+        [InlineData(-2.0)]
+        [InlineData(-4.65)]
+        public void IsLog2Correct(double x)
+        {
+            double y = double.Exp2(x);
+            AssertX.NearlyEqual(x, Quad.Log2(y), Precision.NearestThousandth);
+        }
+
+        [Theory]
+        [InlineData(0.0)]
+        [InlineData(-1.0)]
+        public void IsLog2NaN(double x)
+        {
+            Quad y = Quad.Log2(x);
+            Assert.True(Quad.IsNaN(y));
+        }
+
+        [Theory]
+        [InlineData(-0.288)]
+        [InlineData(0.5)]
+        [InlineData(2.0)]
+        [InlineData(-2.0)]
+        [InlineData(-4.65)]
+        public void IsLog10Correct(double x)
+        {
+            double y = double.Exp10(x);
+            AssertX.NearlyEqual(x, Quad.Log10(y), Precision.NearestThousandth);
+        }
+
+        [Theory]
+        [InlineData(0.0)]
+        [InlineData(-1.0)]
+        public void IsLog10NaN(double x)
+        {
+            Quad y = Quad.Log10(x);
+            Assert.True(Quad.IsNaN(y));
+        }
+
+        [Theory]
+        [InlineData(-0.288, 2.0)]
+        [InlineData(0.5, 2.0)]
+        [InlineData(2.0, 2.0)]
+        [InlineData(-2.0, 2.0)]
+        [InlineData(-4.65, 2.0)]
+        [InlineData(-0.288, 0.5)]
+        [InlineData(0.5, 0.5)]
+        [InlineData(2.0, 0.5)]
+        [InlineData(-2.0, 0.5)]
+        [InlineData(-4.65, 0.5)]
+        public void IsLogBCorrect(double x, double b)
+        {
+            double y = double.Pow(b, x);
+            AssertX.NearlyEqual(x, Quad.Log(y, b), Precision.NearestThousandth);
+        }
+
+        [Theory]
+        [InlineData(0.0, 2.0)]
+        [InlineData(-1.0, 2.0)]
+        [InlineData(0.0, 0.5)]
+        [InlineData(-1.0, 0.5)]
+        [InlineData(1.0, 0.0)]
+        [InlineData(2.0, 0.0)]
+        [InlineData(3.0, 0.0)]
+        [InlineData(1.0, -1.0)]
+        [InlineData(2.0, -1.0)]
+        [InlineData(3.0, -1.0)]
+        public void IsLogBNaN(double x, double b)
+        {
+            Quad y = Quad.Log(x, b);
+            Assert.True(Quad.IsNaN(y));
+        }
+    }
+}
