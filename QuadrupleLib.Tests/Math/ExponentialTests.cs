d7c1c12a0250ef957a8b43c84beb1ae0667e06ff
Isabelle Santin <isabelle@chosenfewsoftware.com>
2026-03-13 14:01:35 -0500

    Fix: add `ExponentialTests` class

---
+++ QuadrupleLib.Tests/Math/ExponentialTests.cs
@@ -0,0 +1,79 @@
+﻿/*
+ *  Copyright 2026 Chosen Few Software
+ *  This file is part of QuadrupleLib.
+ *
+ *  QuadrupleLib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QuadrupleLib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with QuadrupleLib.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using QuadrupleLib.Tests.Assertions.Types;
+using Xunit;
+
+namespace QuadrupleLib.Tests.Math
+{
+    public class ExponentialTests
+    {
+        [Theory]
+        [InlineData(-0.288)]
+        [InlineData(0.5)]
+        [InlineData(2.0)]
+        [InlineData(-2.0)]
+        [InlineData(-4.65)]
+        public void IsExpCorrect(double x)
+        {
+            double y = double.Exp(x);
+            AssertX.NearlyEqual(y, Quad.Exp(x), Precision.NearestThousandth);
+        }
+
+        [Theory]
+        [InlineData(-0.288)]
+        [InlineData(0.5)]
+        [InlineData(2.0)]
+        [InlineData(-2.0)]
+        [InlineData(-4.65)]
+        public void IsExp2Correct(double x) 
+        {
+            double y = double.Exp2(x);
+            AssertX.NearlyEqual(y, Quad.Exp2(x), Precision.NearestThousandth);
+        }
+
+        [Theory]
+        [InlineData(-0.288)]
+        [InlineData(0.5)]
+        [InlineData(2.0)]
+        [InlineData(-2.0)]
+        [InlineData(-4.65)]
+        public void IsExp10Correct(double x)
+        {
+            double y = double.Exp10(x);
+            AssertX.NearlyEqual(y, Quad.Exp10(x), Precision.NearestThousandth);
+        }
+
+        [Theory]
+        [InlineData(-0.288, 2.0)]
+        [InlineData(0.5, 2.0)]
+        [InlineData(2.0, 2.0)]
+        [InlineData(-2.0, 2.0)]
+        [InlineData(-4.65, 2.0)]
+        [InlineData(-0.288, 0.5)]
+        [InlineData(0.5, 0.5)]
+        [InlineData(2.0, 0.5)]
+        [InlineData(-2.0, 0.5)]
+        [InlineData(-4.65, 0.5)]
+        public void IsPowCorrect(double x, double b)
+        {
+            double y = double.Pow(b, x);
+            AssertX.NearlyEqual(y, Quad.Pow(b, x), Precision.NearestThousandth);
+        }
+    }
+}
